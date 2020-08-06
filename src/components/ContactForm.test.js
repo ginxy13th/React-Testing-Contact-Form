674b5dd648437e88617ee2072ed6c233d1b00bad
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-06 16:43:05 -0400

    removed error in terminal

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 test("renders App without crashing", () => {
   render(<ContactForm />);
 });
 
-test("adds input to screen after filling form and submitting", () => {
+test("adds input to screen after filling form and submitting", async () => {
     render(<ContactForm />)
     // const firstNameInput = screen.getByLabelText('First Name*');
     const firstNameInput = screen.getByPlaceholderText('Edd')
@@ -16,16 +16,18 @@ test("adds input to screen after filling form and submitting", () => {
     const messageInput = screen.getByText('Message');
     const submitButton = screen.getByRole("button", { name: "Submit"})
 
-    
-       fireEvent.change(firstNameInput, { target: { value: 'elizabeth' } });
+    await act( async() => {
+      fireEvent.change(firstNameInput, { target: { value: 'elizabeth' } });
         fireEvent.change(lastNameInput, { target: { value: 'bailey' } });
         fireEvent.change(ageInput, {target: {value: 32}})
         fireEvent.change(emailInput, { target: { ref: 'email@email.com' } });
         fireEvent.change(messageInput, { target: { ref: 'some messages' } });
-        fireEvent.click(submitButton) 
+        fireEvent.click(submitButton)   
+    })
+       
        
 
-        expect(screen.getByDisplayValue('elizabeth')).toBeInTheDocument();
+       await expect( screen.getByDisplayValue('elizabeth')).toBeInTheDocument();
    
 })
   
